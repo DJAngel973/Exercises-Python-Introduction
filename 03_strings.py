815b23f4c5fb30d6c1a2121c9ee8d691693d2ab8
DJAngel973 <angel.djuancho@gmail.com>
2025-06-11 21:41:47 -0500

    Se dejan las repuestas mas claras implementando buenas practicas del print

--- 03_strings.py
+++ 03_strings.py
@@ -1,7 +1,8 @@
         ### Strings ###
 #1. Declara una variable text con la frase “Aprendiendo Python” y luego imprime la longitud de la cadena usando len().
 text = "Aprendiendo Python"
-print(len(text))
+text_len = len(text)
+print(text_len)
 
 #2. Concatena dos cadenas: “Hola” y “Python”, y muestra el resultado en una sola línea.
 one_str = "Hola"
@@ -26,22 +27,27 @@ print(c)
 print(d)
 print(e)
 print(f)
+#Para poder desempaquetar los caracteres de la palabra es importante crear la misma cantidad de variable que van a tomar esa cantidad de caracteres, en este caso se necesitaron 6 variables para poder realizar el ejercicio de acuerdo al ejemplo.
 
 #6. Extrae un “slice” de la palabra “Programación” para obtener los caracteres desde la posición 3 hasta la 7.
-word_slice = "Programación"
-print(word_slice[3:7])
+first_word = "Programación"
+word_slice = first_word[3:7]
+print(f"Una parte de la palabra '{first_word}', posiciones 3 al 7: {word_slice}.")
 
 #7. Invierte la cadena “Python” usando slicing y muestra el resultado.
 reversed_two_str = two_str[::-1]
-print(reversed_two_str)
+print(f"La palabra 'Python' invertida: {reversed_two_str}")
 
 #8. Convierte la cadena “aprendiendo python” en mayúsculas usando el método adecuado e imprímela.
-print(text.upper())
+capital_letters = text.upper()
+print(f"Convirtiendo en mayúsculas 'aprendiendo python': {capital_letters}")
 
 #9. Cuenta cuántas veces aparece la letra “n” en la cadena “Programación en Python”.
 text_two = "Programación en Python"
-print(f'Cuantas n tiene la cadena de texto "Programación en Python": {text_two.count("n")}')
+text_two_count = text_two.count("n")
+print(f'Cuantas n tiene la cadena de texto "Programación en Python": {text_two_count}')
 
 #10. Verifica si la cadena “12345” es numérica usando el método adecuado e imprime el resultado.
 three_str = "12345"
-print(f'La cadena de texto "12345" es numérica: {three_str.isnumeric()}')
+three_str_is_number = three_str.isnumeric()
+print(f'La cadena de texto "12345" es numérica: {three_str_is_number}')
